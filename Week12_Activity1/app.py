235870526cce2a644ad9d5f0ed1ed98ef30f1868
Saksham511 <sakshamshrestha511@gmail.com>
2026-03-08 02:01:08 +0545

    w12a3 added

--- Week12_Activity1/app.py
+++ Week12_Activity1/app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Flask Framework!"
+    return "<p>Hello Flask!</p>"
 
 @app.route("/depart")
 def depart():
     return "<p>See you soon!</p>"
+
+@app.route("/username/<name>")
+def portal(name):
+    return f"{name} is online"
 
 if __name__ == "__main__":
     app.run(debug=True)
